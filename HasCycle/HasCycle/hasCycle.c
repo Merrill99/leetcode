1cf1be11963f5d4bda0f34113f487facbb501d7c
Merrill99 <merrill_0406@qq.com>
2023-05-05 22:38:14 +0800

    给你一个链表的头节点head，判断链表中是否有环。

---
+++ HasCycle/HasCycle/hasCycle.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<Stdbool.h>
+
+struct ListNode {
+    int val;
+    struct ListNode* next;
+};
+
+bool hasCycle(struct ListNode* head) {
+    struct ListNode* slow = head, * fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
